f624869384eb393a2e14e215aa830fe5d85b8a75
jkot <jkot25@gmail.com>
2020-11-13 01:33:39 +0300

    +

--- src/gapi/Texture.cpp
+++ src/gapi/Texture.cpp
@@ -22,31 +22,32 @@ namespace OpenDemo
             ASSERT(desc_.sampleCount > 0)
             ASSERT(desc_.arraySize > 0)
 
+            ASSERT(
+                (desc_.sampleCount > 1 && desc_.type == Type::Texture2DMS)
+                || (desc_.sampleCount == 1 && desc_.type != Type::Texture2DMS));
+
             switch (desc_.type)
             {
             case Type::Texture1D:
-                ASSERT(desc_.height == 1 && desc_.depth == 1 && desc_.sampleCount == 1)
+                ASSERT(desc_.height == 1)
+                ASSERT(desc_.depth == 1)
                 break;
             case Type::Texture2D:
-                ASSERT(desc_.depth == 1 && desc_.sampleCount == 1)
-                break;
             case Type::Texture2DMS:
-                ASSERT(desc_.depth == 1 && desc_.sampleCount > 1)
+            case Type::TextureCube:
+                ASSERT(desc_.depth == 1)
                 break;
             case Type::Texture3D:
-                ASSERT(desc_.arraySize == 1 && desc_.sampleCount == 1)
-                break;
-            case Type::TextureCube:
-                ASSERT(desc_.depth == 1 && desc_.sampleCount == 1)
+                ASSERT(desc_.arraySize == 1)
                 break;
             default:
-                ASSERT_MSG(false, "Wrong texture type");
+                LOG_FATAL("Unsupported texture type");
             }
-            
+
             // TODO add mipLevels calculation;
         }
 
-        RenderTargetView::SharedPtr Texture::GetRTV(uint32_t mipLevel, uint32_t firstArraySlice, uint32_t arraySize) 
+        RenderTargetView::SharedPtr Texture::GetRTV(uint32_t mipLevel, uint32_t firstArraySlice, uint32_t arraySize)
         {
             return RenderTargetView::Create(std::dynamic_pointer_cast<Resource>(shared_from_this()), name_);
         }
--- src/gapi_dx12/ResourceImpl.cpp
+++ src/gapi_dx12/ResourceImpl.cpp
@@ -21,6 +21,28 @@ namespace OpenDemo
                 ASSERT(resourceDesc.sampleCount > 0)
                 ASSERT(resourceDesc.arraySize > 0)
 
+                ASSERT(
+                    (resourceDesc.sampleCount > 1 && resourceDesc.type == Texture::Type::Texture2DMS)
+                    || (resourceDesc.sampleCount == 1 && resourceDesc.type != Texture::Type::Texture2DMS));
+
+                switch (resourceDesc.type)
+                {
+                case Texture::Type::Texture1D:
+                    ASSERT(resourceDesc.height == 1)
+                    ASSERT(resourceDesc.depth == 1)
+                    break;
+                case Texture::Type::Texture2D:
+                case Texture::Type::Texture2DMS:
+                case Texture::Type::TextureCube:
+                    ASSERT(resourceDesc.depth == 1)
+                    break;
+                case Texture::Type::Texture3D:
+                    ASSERT(resourceDesc.arraySize == 1)
+                    break;
+                default:
+                    LOG_FATAL("Unsupported texture type");
+                }
+
                 D3D12_HEAP_FLAGS heapFlags = D3D12_HEAP_FLAG_NONE;
                 D3D12_RESOURCE_STATES initialResourceState;
                 D3D12_CLEAR_VALUE optimizedClearValue;
@@ -32,33 +54,18 @@ namespace OpenDemo
                 switch (resourceDesc.type)
                 {
                 case Texture::Type::Texture1D:
-                    ASSERT(resourceDesc.height == 1 && resourceDesc.depth == 1 && resourceDesc.sampleCount == 1)
-
                     desc = CD3DX12_RESOURCE_DESC::Tex1D(format, resourceDesc.width, resourceDesc.arraySize, resourceDesc.mipLevels);
                     break;
-
                 case Texture::Type::Texture2D:
                 case Texture::Type::Texture2DMS:
-                    ASSERT(resourceDesc.depth == 1)
-                    ASSERT(
-                        (resourceDesc.sampleCount > 1 && resourceDesc.type == Texture::Type::Texture2DMS)
-                        || (resourceDesc.sampleCount == 1 && resourceDesc.type == Texture::Type::Texture2D))
-
                     desc = CD3DX12_RESOURCE_DESC::Tex2D(format, resourceDesc.width, resourceDesc.height, resourceDesc.arraySize, resourceDesc.mipLevels, resourceDesc.sampleCount);
                     break;
-
                 case Texture::Type::Texture3D:
-                    ASSERT(resourceDesc.arraySize == 1 && resourceDesc.sampleCount == 1)
-
                     desc = CD3DX12_RESOURCE_DESC::Tex3D(format, resourceDesc.width, resourceDesc.height, resourceDesc.depth, resourceDesc.mipLevels);
                     break;
-
                 case Texture::Type::TextureCube:
-                    ASSERT(resourceDesc.depth == 1 && resourceDesc.sampleCount == 1)
-
                     desc = CD3DX12_RESOURCE_DESC::Tex2D(format, resourceDesc.width, resourceDesc.height, resourceDesc.arraySize * 6, resourceDesc.mipLevels);
                     break;
-
                 default:
                     LOG_FATAL("Unsupported texture type");
                 }
