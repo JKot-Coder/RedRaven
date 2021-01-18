6d36b599f820922d9476e96813843d779a1c6dcc
Konstantin Zhirnokleev <k_zhirnokleev@wargaming.net>
2021-01-18 20:02:50 +0300

    +/- catch2

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,45 +1,83 @@
-#include <boost/ut.hpp>
+#include <catch2/catch_all.hpp>
+    /*
+#include <catch2/internal/catch_default_main.hpp>
 
-namespace ut = boost::ut;
 
-namespace cfg2
+isDebuggerActive
+
+
+
+    Catch::ConfigData config;
+    config.showDurations = Catch::ShowDurations::Always;
+    config.useColour = Catch::UseColour::No;
+    config.outputFilename = "%debug";
+    session.useConfigData( config );
+
+
+
+    return session.run( argc, argv );
+
+
+
+namespace Catch
 {
-    struct printer : ut::printer
-    {
-        template <class T>
-        auto& operator<<(T&& t)
-        {
-            std::cerr << std::forward<T>(t);
-            return *this;
-        }
-    };
-} // namespace cfg
+    CATCH_INTERNAL_START_WARNINGS_SUPPRESSION
+    CATCH_INTERNAL_SUPPRESS_GLOBALS_WARNINGS
+    LeakDetector leakDetector;
+    CATCH_INTERNAL_STOP_WARNINGS_SUPPRESSION
+}
 
-template <>
-auto ut::cfg<ut::override> = ut::runner<ut::reporter<cfg2::printer>> {};
+int executTests(int argc, char** argv)
+{
+    // We want to force the linker not to discard the global variable
+    // and its constructor, as it (optionally) registers leak detector
+    (void)&Catch::leakDetector;
 
-#ifdef OS_WINDOWS
+    return Catch::Session().run(argc, argv);
+}
+
+#if defined(OS_WINDOWS) && defined(UNICODE)
 #include <Windows.h>
-INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+#include <shellapi.h>
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPreInst, LPSTR lpCmdLine, int nCmdShow)
 {
-    (void)hInstance;
-    (void)hPrevInstance;
-    (void)lpCmdLine;
-    (void)nCmdShow;
+    int argc;
+    char** argv;
 
+    LPWSTR* lpArgv = CommandLineToArgvW(GetCommandLineW(), &argc);
+    argv = (char**)malloc(argc * sizeof(char*));
+    for (int i = 0; i < argc; ++i)
+    {
+        const auto size = wcslen(lpArgv[i]) + 1;
+        argv[i] = static_cast<char*>(malloc(size));
+        wcstombs(argv[i], lpArgv[i], size);
+    }
+    LocalFree(lpArgv);
+
+    const auto exitCode = executTests(argc, argv);
+
+    for (int i = 0; i < argc; ++i)
+        free(argv[i]);
+    free(argv);
+
+    return exitCode;
+}
 #else
 int main(int argc, char** argv)
 {
-    (void)argc;
-    (void)argv;
+    return executTests(argc, argv);
+}
 #endif
-    using namespace ut;
-    expect((1 == 2_i)) << "Not enough parameters!";
-
-    return cfg<override>.run();
+*/
+unsigned int Factorial(unsigned int number)
+{
+    return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
-ut::suite basic = [] {
-    using namespace ut;
-    should("equal") = [] { expect(43_i == 42); };
-};
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+    REQUIRE(Factorial(1) == 1);
+    REQUIRE(Factorial(2) == 2);
+    REQUIRE(Factorial(3) == 6);
+    REQUIRE(Factorial(10) == 3628800);
+}
