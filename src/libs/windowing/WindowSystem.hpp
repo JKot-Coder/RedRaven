028f2c255379a4434d3e75bbba622fc199e823a6
jkot <jkot25@gmail.com>
2021-06-07 12:25:48 +0300

    +

--- src/libs/windowing/WindowSystem.hpp
+++ src/libs/windowing/WindowSystem.hpp
@@ -9,7 +9,7 @@ namespace RR
     namespace Windowing
     {
 #ifdef OS_WINDOWS
-        constexpr wchar_t WINDOW_CLASS_NAME[] = L"OpenDemoWndClass";
+        constexpr wchar_t WINDOW_CLASS_NAME[] = L"RedRevenWndClass";
 #endif
 
         class WindowSystem final : public Singleton<WindowSystem>
