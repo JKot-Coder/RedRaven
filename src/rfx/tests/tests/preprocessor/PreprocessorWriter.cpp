f74cfb9b0ea3b9ee952d17a4784e7cfa7b7b7aed
jkot <jkot25@gmail.com>
2022-02-06 15:59:15 +0300

    +

---
+++ src/rfx/tests/tests/preprocessor/PreprocessorWriter.cpp
@@ -0,0 +1,74 @@
+#include "PreprocessorWriter.hpp"
+
+#include "core/StringEscapeUtil.hpp"
+
+namespace RR::Rfx
+{
+    namespace Tests
+    {
+        namespace
+        {
+
+            void writeDiagnosticLog(std::ofstream& ofs, const std::shared_ptr<BufferWriter>& disagnosticBuffer)
+            {
+                ofs << "\tDiagnostic:[\n";
+
+                U8String line;
+                std::istringstream logStream(disagnosticBuffer->GetBuffer());
+
+                while (std::getline(logStream, line, '\n'))
+                {
+                    U8String escapedLine;
+                    StringEscapeUtil::AppendEscaped(StringEscapeUtil::Style::JSON, line, escapedLine);
+
+                    ofs << fmt::format("\t\t\"{}\",\n", escapedLine);
+                }
+
+                ofs << "\t],";
+            }
+
+            void writeTokens(std::ofstream& ofs, const std::shared_ptr<Lexer>& lexer)
+            {
+                ofs << "\tTokens:[\n";
+
+                while (true)
+                {
+                    const auto& token = lexer->ReadToken();
+
+                    U8String escapedToken;
+                    StringEscapeUtil::AppendEscaped(StringEscapeUtil::Style::JSON, token.stringSlice, escapedToken);
+
+                    ofs << fmt::format("\t\t{{Type:\"{0}\", Content:\"{1}\", Line:{2}, Column:{3}}},\n",
+                                       RR::Rfx::TokenTypeToString(token.type),
+                                       escapedToken,
+                                       token.humaneSourceLocation.line,
+                                       token.humaneSourceLocation.column);
+
+                    if (token.type == TokenType::EndOfFile)
+                        break;
+                }
+
+                ofs << "\t],";
+            }
+
+        }
+
+        void LexerWriter::write(std::string path) const
+        {
+            std::ofstream ofs(path);
+            ASSERT(ofs.is_open())
+
+            ofs << "{\n";
+
+            writeTokens(ofs, lexer_);
+
+            ofs << "\n";
+
+            writeDiagnosticLog(ofs, disagnosticBuffer_);
+
+            ofs << "\n}";
+
+            ofs.close();
+        }
+    }
+}
---
+++ src/rfx/tests/tests/preprocessor/PreprocessorWriter.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "compiler/DiagnosticSink.hpp"
+#include "compiler/Lexer.hpp"
+
+#include "ApprovalTests/ApprovalTests.hpp"
+
+namespace RR::Rfx
+{
+    namespace Tests
+    {
+        class LexerWriter : public ApprovalTests::ApprovalWriter
+        {
+        public:
+            explicit LexerWriter(const std::shared_ptr<Lexer>& lexer, const std::shared_ptr<BufferWriter>& disagnosticBuffer)
+                : lexer_(lexer), disagnosticBuffer_(disagnosticBuffer)
+            {
+            }
+
+            std::string getFileExtensionWithDot() const override
+            {
+                return ".tokens";
+            }
+
+            void write(std::string path) const override;
+
+            void cleanUpReceived(std::string receivedPath) const override
+            {
+                remove(receivedPath.c_str());
+            }
+
+        private:
+            std::shared_ptr<Lexer> lexer_;
+            std::shared_ptr<BufferWriter> disagnosticBuffer_;
+        };
+    }
+}
