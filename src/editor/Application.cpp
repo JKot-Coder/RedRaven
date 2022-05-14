264ebaffa484630e5c6e3bab178f488522554481
jkot <jkot25@gmail.com>
2022-05-14 21:08:06 +0300

    switch to glfw

--- src/editor/Application.cpp
+++ src/editor/Application.cpp
@@ -3,14 +3,16 @@
 #include <Windows.h>
 
 #include "backends/imgui_impl_dx12.h"
-#include "backends/imgui_impl_win32.h"
+#include "backends/imgui_impl_glfw.h"
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <imgui.h>
 #include <tchar.h>
 
-// Forward declare message handler from imgui_impl_win32.cpp
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+// GLFW
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>  
 
 namespace RR
 {
@@ -295,61 +297,35 @@ namespace RR
             return frameCtx;
         }
 
-        // Win32 message handler
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+        static void glfw_error_callback(int error, const char* description)
         {
-            if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-                return true;
-
-            switch (msg)
-            {
-                case WM_SIZE:
-                    if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
-                    {
-                        WaitForLastSubmittedFrame();
-                        CleanupRenderTarget();
-                        HRESULT result = g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT);
-                        assert(SUCCEEDED(result) && "Failed to resize swapchain.");
-                        CreateRenderTarget();
-                    }
-                    return 0;
-                case WM_SYSCOMMAND:
-                    if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-                        return 0;
-                    break;
-                case WM_DESTROY:
-                    ::PostQuitMessage(0);
-                    return 0;
-            }
-            return ::DefWindowProc(hWnd, msg, wParam, lParam);
+            fprintf(stderr, "Glfw Error %d: %s\n", error, description);
         }
+
     }
 
     int Application::Run()
     {
         init();
 
-        // Create application window
-        //ImGui_ImplWin32_EnableDpiAwareness();
-        WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
-        ::RegisterClassEx(&wc);
-        HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("Dear ImGui DirectX12 Example"), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+        // Setup window
+        glfwSetErrorCallback(glfw_error_callback);
+        if (!glfwInit())
+            return 1;
+
+        // Create window with graphics context
+        GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+        if (window == NULL)
+            return 1;
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1); // Enable vsync
 
         // Initialize Direct3D
-        if (!CreateDeviceD3D(hwnd))
+        if (!CreateDeviceD3D(glfwGetWin32Window(window)))
         {
             CleanupDeviceD3D();
-            ::UnregisterClass(wc.lpszClassName, wc.hInstance);
             return 1;
         }
-
-        // Show the window
-        ::ShowWindow(hwnd, SW_SHOWDEFAULT);
-        ::UpdateWindow(hwnd);
 
         // Setup Dear ImGui context
         IMGUI_CHECKVERSION();
@@ -376,7 +352,7 @@ namespace RR
         }
 
         // Setup Platform/Renderer backends
-        ImGui_ImplWin32_Init(hwnd);
+        ImGui_ImplGlfw_InitForOther(window, true);
         ImGui_ImplDX12_Init(g_pd3dDevice, NUM_FRAMES_IN_FLIGHT,
                             DXGI_FORMAT_R8G8B8A8_UNORM, g_pd3dSrvDescHeap,
                             g_pd3dSrvDescHeap->GetCPUDescriptorHandleForHeapStart(),
@@ -421,7 +397,7 @@ namespace RR
 
             // Start the Dear ImGui frame
             ImGui_ImplDX12_NewFrame();
-            ImGui_ImplWin32_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
             // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
@@ -511,12 +487,12 @@ namespace RR
 
         // Cleanup
         ImGui_ImplDX12_Shutdown();
-        ImGui_ImplWin32_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
         ImGui::DestroyContext();
 
         CleanupDeviceD3D();
-        ::DestroyWindow(hwnd);
-        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
+        glfwDestroyWindow(window);
+        glfwTerminate();
 
         return 0;
     }
