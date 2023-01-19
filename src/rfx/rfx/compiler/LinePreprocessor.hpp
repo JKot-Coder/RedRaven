2f0f27c0ba8a2b77166895411530bbddda4bda83
Константин Жирноклеев <jkot25@gmail.com>
2023-01-20 00:46:40 +0400

    LinePreprocessor

---
+++ src/rfx/rfx/compiler/LinePreprocessor.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace RR
+{
+    namespace Common
+    {
+        class LinearAllocator;
+    }
+
+    namespace Rfx
+    {
+        class SourceFile;
+        class DiagnosticSink;
+        struct Token;
+        class LinePreprocessorImpl;
+
+        class LinePreprocessor final
+        {
+        public:
+            LinePreprocessor() = delete;
+            LinePreprocessor(const std::shared_ptr<Common::LinearAllocator>& allocator,
+                             const std::shared_ptr<DiagnosticSink>& diagnosticSink);
+
+            ~LinePreprocessor();
+
+            void PushInputFile(const std::shared_ptr<SourceFile>& sourceFile);
+            void DefineMacro(const U8String& key, const U8String& value);
+
+            // read the entire input into tokens
+            std::vector<Token> ReadAllTokens();
+
+        private:
+            // TODO tempoprary shared. Is it possible not to use it?
+            std::shared_ptr<LinePreprocessorImpl> impl_;
+        };
+    }
+}
