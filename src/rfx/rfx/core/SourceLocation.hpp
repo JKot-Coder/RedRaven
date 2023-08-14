789baf26c4f017391a9edb8bc2cdfb4f7ef621bd
Константин Жирноклеев <jkot25@gmail.com>
2023-08-15 00:05:03 +0300

    Path info upd

--- src/rfx/rfx/core/SourceLocation.hpp
+++ src/rfx/rfx/core/SourceLocation.hpp
@@ -66,8 +66,6 @@ namespace RR
 
         struct PathInfo
         {
-            typedef PathInfo ThisType;
-
             /// To be more rigorous about where a path comes from, the type identifies what a paths origin is
             enum class Type : uint8_t
             {
@@ -76,7 +74,6 @@ namespace RR
                 FoundPath, ///< Just has a found path (uniqueIdentity is unknown, or even 'unknowable')
                 FromString, ///< Created from a string (so found path might not be defined and should not be taken as to map to a loaded file)
                 TokenPaste, ///< No paths, just created to do a macro expansion
-                TypeParse, ///< No path, just created to do a type parse
                 CommandLine, ///< A macro constructed from the command line
 
                 Split,
@@ -89,19 +86,19 @@ namespace RR
             /// True if has a found path that has originated from a file (as opposed to string or some other origin)
             inline bool hasFileFoundPath() const { return (type == Type::Normal || type == Type::FoundPath) && foundPath.length() > 0; }
 
-            bool operator==(const ThisType& rhs) const;
-            bool operator!=(const ThisType& rhs) const { return !(*this == rhs); }
+            bool operator==(const PathInfo& rhs) const;
+            bool operator!=(const PathInfo& rhs) const { return !(*this == rhs); }
 
             /// Returns the 'most unique' identity for the path. If has a 'uniqueIdentity' returns that, else the foundPath, else "".
             const U8String getMostUniqueIdentity() const;
 
+            // So simplify construction. In normal usage it's safer to use make methods over constructing directly.
             static PathInfo makeSplit(const U8String& foundPathIn, const U8String& uniqueIdentity)
             {
                 ASSERT(uniqueIdentity.length() > 0 && foundPathIn.length() > 0)
                 return PathInfo { Type::Split, foundPathIn, uniqueIdentity };
             }
 
-            // So simplify construction. In normal usage it's safer to use make methods over constructing directly.
             static PathInfo makeUnknown() { return PathInfo { Type::Unknown, U8String(), U8String() }; }
             static PathInfo makeTokenPaste() { return PathInfo { Type::TokenPaste, "token paste", U8String() }; }
             static PathInfo makeNormal(const U8String& foundPathIn, const U8String& uniqueIdentity)
@@ -114,11 +111,10 @@ namespace RR
                 ASSERT(pathIn.length() > 0)
                 return PathInfo { Type::FoundPath, pathIn, U8String() };
             }
-            static PathInfo makeTypeParse() { return PathInfo { Type::TypeParse, "type string", U8String() }; }
             static PathInfo makeCommandLine() { return PathInfo { Type::CommandLine, "command line", U8String() }; }
             static PathInfo makeFromString(const U8String& userPath) { return PathInfo { Type::FromString, userPath, U8String() }; }
 
-            Type type; ///< The type of path
+            Type type = Type::Unknown; ///< The type of path
             U8String foundPath; ///< The path where the file was found (might contain relative elements)
             U8String uniqueIdentity; ///< The unique identity of the file on the path found
         };
