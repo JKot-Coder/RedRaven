981dc9ac3e9a8c32043e3e4a248591243acd3236
jkot <jkot25@gmail.com>
2024-05-05 13:19:45 +0300

    Proper ComPtr impl

--- src/libs/common/ComPtr.hpp
+++ src/libs/common/ComPtr.hpp
@@ -6,44 +6,53 @@
 #define NOINLINE __attribute__((noinline))
 #endif
 
+#include <Guiddef.h>
+#include <unknwn.h>
+
 namespace RR::Common
 {
-    template <typename T, typename Enable = void>
-    struct abi
+    template <typename T>
+    struct ComPtr;
+
+    namespace impl
     {
-        using type = T;
-    };
+        template <typename T, typename Enable = void>
+        struct abi
+        {
+            using type = T;
+        };
+
+        template <typename T>
+        struct abi<T, std::enable_if_t<std::is_enum_v<T>>>
+        {
+            using type = std::underlying_type_t<T>;
+        };
+
+        template <typename T>
+        using abi_t = typename abi<T>::type;
+
+        struct take_ownership_from_abi_t
+        {
+        };
+        inline constexpr take_ownership_from_abi_t take_ownership_from_abi {};
+
+    }
 
     template <typename T>
-    struct abi<T, std::enable_if_t<std::is_enum_v<T>>>
+    struct ComPtr
     {
-        using type = std::underlying_type_t<T>;
-    };
-
-    template <typename T>
-    using abi_t = typename abi<T>::type;
-
-    template <typename T>
-    class ComPtr
-    {
-    public:
-        using Type = abi_t<T>;
-
-        ~ComPtr() noexcept { releaseRef(); }
+        using type = impl::abi_t<T>;
 
         ComPtr(std::nullptr_t = nullptr) noexcept { }
-        ComPtr(void* ptr) noexcept : ptr_(static_cast<Type*>(ptr)) { addRef(); }
-        ComPtr(ComPtr const& other) noexcept : ptr_(other.ptr_) { addRef(); }
-
-        template <typename U>
-        ComPtr(ComPtr<U> const& other) noexcept : ptr_(other.ptr_) { addRef(); }
-
-        template <typename U>
-        ComPtr(ComPtr<U>&& other) noexcept : ptr_(std::exchange(other.ptr_, {})) { }
+        ComPtr(void* ptr, impl::take_ownership_from_abi_t) noexcept : ptr_(static_cast<type*>(ptr)) { }
+        ComPtr(ComPtr const& other) noexcept : ptr_(other.ptr_) { add_ref(); }
+        template <typename U> ComPtr(ComPtr<U> const& other) noexcept : ptr_(other.ptr_) { add_ref(); }
+        template <typename U> ComPtr(ComPtr<U>&& other) noexcept : ptr_(std::exchange(other.ptr_, {})) { }
+        ~ComPtr() noexcept { release_ref(); }
 
         ComPtr& operator=(ComPtr const& other) noexcept
         {
-            copyRef(other.ptr_);
+            copy_ref(other.ptr_);
             return *this;
         }
 
@@ -51,7 +60,7 @@ namespace RR::Common
         {
             if (this != &other)
             {
-                releaseRef();
+                release_ref();
                 ptr_ = std::exchange(other.ptr_, {});
             }
 
@@ -61,14 +70,14 @@ namespace RR::Common
         template <typename U>
         ComPtr& operator=(ComPtr<U> const& other) noexcept
         {
-            copyRef(other.ptr_);
+            copy_ref(other.ptr_);
             return *this;
         }
 
         template <typename U>
         ComPtr& operator=(ComPtr<U>&& other) noexcept
         {
-            releaseRef();
+            release_ref();
             ptr_ = std::exchange(other.ptr_, {});
             return *this;
         }
@@ -76,53 +85,62 @@ namespace RR::Common
         explicit operator bool() const noexcept { return ptr_ != nullptr; }
         auto operator->() const noexcept { return ptr_; }
         T& operator*() const noexcept { return *ptr_; }
-
-        Type* get() const noexcept { return ptr_; }
-        Type** put() noexcept
+        type* get() const noexcept { return ptr_; }
+        type** put() noexcept
         {
-            assert(ptr_ == nullptr);
+            ASSERT(ptr_ == nullptr);
             return &ptr_;
         }
 
         void** put_void() noexcept { return reinterpret_cast<void**>(put()); }
-
-        void attach(Type* value) noexcept
+        void attach(type* value) noexcept
         {
-            releaseRef();
+            release_ref();
             *put() = value;
         }
+        type* detach() noexcept { return std::exchange(ptr_, {}); }
+        friend void swap(ComPtr& left, ComPtr& right) noexcept { std::swap(left.ptr_, right.ptr_); }
 
-        Type* detach() noexcept { return std::exchange(ptr_, {}); }
-
-        friend void swap(ComPtr& left, ComPtr& right) noexcept
+        template <typename To>
+        ComPtr<To> as() const noexcept
         {
-            std::swap(left.ptr_, right.ptr_);
+            if (!ptr_)
+                return nullptr;
+
+            void* result {};
+            ASSERT(SUCCEEDED(ptr_->QueryInterface(__uuidof(To), &result)));
+            return ComPtr<To>(result, impl::take_ownership_from_abi);
         }
 
-        void copyTo(Type** other) const noexcept
+        template <typename To>
+        HRESULT try_as(ComPtr<To>& p) const noexcept
         {
-            addRef();
-            *other = ptr_;
+            return ptr_->QueryInterface(__uuidof(To), p.put_void());
+        }
+
+        HRESULT try_as(REFIID id, void** result) const noexcept
+        {
+            return ptr_->QueryInterface(id, result);
         }
 
     private:
-        void copyRef(Type* other) noexcept
+        void copy_ref(type* other) noexcept
         {
             if (ptr_ != other)
             {
-                releaseRef();
+                release_ref();
                 ptr_ = other;
-                addRef();
+                add_ref();
             }
         }
 
-        void addRef() const noexcept
+        void add_ref() const noexcept
         {
             if (ptr_)
-                const_cast<std::remove_const_t<Type>*>(ptr_)->AddRef();
+                const_cast<std::remove_const_t<type>*>(ptr_)->AddRef();
         }
 
-        void releaseRef() noexcept
+        void release_ref() noexcept
         {
             if (ptr_)
                 unconditional_release_ref();
@@ -133,70 +151,11 @@ namespace RR::Common
             std::exchange(ptr_, {})->Release();
         }
 
-        template <typename>
-        friend class ComPtr;
-        Type* ptr_ {};
+        template <typename U>
+        friend struct ComPtr;
+
+        type* ptr_ {};
     };
-
-    template <typename T>
-    bool operator==(ComPtr<T> const& left, ComPtr<T> const& right) noexcept
-    {
-        return left.get() == right.get();
-    }
-
-    template <typename T>
-    bool operator==(ComPtr<T> const& left, std::nullptr_t) noexcept
-    {
-        return left.get() == nullptr;
-    }
-
-    template <typename T>
-    bool operator==(std::nullptr_t, ComPtr<T> const& right) noexcept
-    {
-        return right.get();
-    }
-
-    template <typename T>
-    bool operator!=(ComPtr<T> const& left, ComPtr<T> const& right) noexcept
-    {
-        return !(left == right);
-    }
-
-    template <typename T>
-    bool operator!=(ComPtr<T> const& left, std::nullptr_t) noexcept
-    {
-        return !(left == nullptr);
-    }
-
-    template <typename T>
-    bool operator!=(std::nullptr_t, ComPtr<T> const& right) noexcept
-    {
-        return !(nullptr == right);
-    }
-
-    template <typename T>
-    bool operator<(ComPtr<T> const& left, ComPtr<T> const& right) noexcept
-    {
-        return left.get() < right.get();
-    }
-
-    template <typename T>
-    bool operator>(ComPtr<T> const& left, ComPtr<T> const& right) noexcept
-    {
-        return right < left;
-    }
-
-    template <typename T>
-    bool operator<=(ComPtr<T> const& left, ComPtr<T> const& right) noexcept
-    {
-        return !(right < left);
-    }
-
-    template <typename T>
-    bool operator>=(ComPtr<T> const& left, ComPtr<T> const& right) noexcept
-    {
-        return !(left < right);
-    }
 }
 
 #undef NOINLINE
