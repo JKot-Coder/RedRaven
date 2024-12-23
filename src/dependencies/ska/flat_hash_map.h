447f4e3d309a18cc0c96d4569eea6847d1eb794c
Konstantin <jkot25@gmail.com>
2024-12-23 05:15:05 +0400

    Add comment about ska flat hash map implemetation

--- src/dependencies/ska/flat_hash_map.h
+++ src/dependencies/ska/flat_hash_map.h
@@ -2,6 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See http://www.boost.org/LICENSE_1_0.txt)
 
+// Modified. Moved to eastl and improved log2 impl
+
 #pragma once
 
 #include <cstdint>
