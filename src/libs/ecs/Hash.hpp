ddccc0df4572c0bb397996ece059da02d4cf8321
Konstantin <jkot25@gmail.com>
2025-01-04 06:24:17 +0400

    ECS: Hash impl

--- src/libs/ecs/Hash.hpp
+++ src/libs/ecs/Hash.hpp
@@ -1,30 +1,52 @@
 
 #pragma once
 
-#include "common/hash/Wyhash.hpp"
 #include "EASTL/fixed_string.h"
+#include "common/hash/Wyhash.hpp"
 
 namespace RR::Ecs
 {
+    template <int FixedSize>
+    struct HashString;
+
     using HashType = uint64_t;
+    using HashName = HashString<64>;
 
-    template<typename stringType>
-    constexpr inline HashType Hash(const stringType& string) {return RR::Common::Hash::Wyhash::Hash<64>(string.data(), (uint32_t)string.size()); }
-    constexpr inline HashType Hash(const char* cstr) { return RR::Common::Hash::Wyhash::Hash<64>(cstr, (uint32_t)std::char_traits<char>::length(cstr)); }
-    constexpr inline HashType Hash(const void* data, std::size_t len) { return RR::Common::Hash::Wyhash::Hash<64>(data, (uint32_t)len); }
-
-    constexpr inline HashType operator"" _h(const char* str, std::size_t len)
+    namespace details
     {
-        return RR::Common::Hash::Wyhash::ForceConstexprHash<64>(str, (uint32_t)len);
+        constexpr inline HashType HashImpl(const char* str, uint32_t len) { return RR::Common::Hash::Wyhash::Hash<64>(str, len); }
+        constexpr inline HashType HashImpl(const void* data, uint32_t len) { return RR::Common::Hash::Wyhash::Hash<64>(data, len); }
+        constexpr inline HashType ConstexprHashImpl(const char* str, uint32_t len) { return RR::Common::Hash::Wyhash::ForceConstexprHash<64>(str, len); }
     }
 
-    template<int FixedSize>
+    template <typename stringType>
+    constexpr inline HashType Hash(const stringType& string) { return details::HashImpl(string.data(), (uint32_t)string.size()); }
+    constexpr inline HashType Hash(const char* cstr) { return details::HashImpl(cstr, (uint32_t)std::char_traits<char>::length(cstr)); }
+    constexpr inline HashType Hash(const void* data, std::size_t len) { return details::HashImpl(data, (uint32_t)len); }
+    template <typename stringType>
+    constexpr inline HashType ConstexprHash(const stringType& string) { return details::ConstexprHashImpl(string.data(), (uint32_t)string.size()); }
+    constexpr inline HashType ConstexprHash(const char* cstr) { return details::ConstexprHashImpl(cstr, (uint32_t)std::char_traits<char>::length(cstr)); }
+    constexpr inline HashType ConstexprHash(const char* str, std::size_t len) { return details::ConstexprHashImpl(str, (uint32_t)len); }
+
+    constexpr inline HashType operator"" _h(const char* str, std::size_t len) { return details::ConstexprHashImpl(str, (uint32_t)len); }
+
+    template <int FixedSize>
     struct HashString
     {
         template <typename stringType>
         constexpr inline HashString(const stringType& str) : string(str.data(), str.size()), hash(Hash(str)) { }
         constexpr inline HashString(const char* cstr) : string(cstr), hash(Hash(cstr)) { }
         constexpr inline HashString() = default;
+
+        constexpr bool operator==(const HashString& other) const
+        {
+            return hash == other.hash;
+        }
+
+        constexpr operator HashType() const noexcept
+        {
+            return hash;
+        }
 
         eastl::fixed_string<char, FixedSize> string;
         HashType hash;
