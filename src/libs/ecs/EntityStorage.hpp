dfbdaa3378a5ecb3afae2b64c795395d1ed710cb
Konstantin <jkot25@gmail.com>
2025-01-04 06:20:40 +0400

    ECS: Entity Storage initial impl

---
+++ src/libs/ecs/EntityStorage.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "ecs/Id.hpp"
+
+namespace RR::Ecs
+{
+    struct EntityRecord
+    {
+        uint32_t generation;
+    };
+
+    struct EntityStorage
+    {
+        eastl::vector<EntityRecord> entityRecords;
+        eastl::vector<uint32_t> freeIndices;
+
+        EntityId Create()
+        {
+            EntityId entityId;
+            if (freeIndices.empty())
+            {
+                entityId = EntityId(entityRecords.size(), 0);
+                entityRecords.push_back({entityId.GetGeneration()});
+            }
+            else
+            {
+                uint32_t entityIndex = freeIndices.back();
+                freeIndices.pop_back();
+
+                uint32_t generation = entityRecords[entityIndex].generation;
+                entityId = EntityId(entityIndex, generation);
+                entityRecords[entityIndex] = {generation};
+            }
+            return entityId;
+        }
+
+        bool IsAlive(EntityId entityId) const
+        {
+            return entityId.GetEntityIndex() < entityRecords.size() && entityRecords[entityId.GetEntityIndex()].generation == entityId.GetGeneration();
+        }
+
+        void Destroy(EntityId entityId)
+        {
+            if (IsAlive(entityId))
+            {
+                auto& entityRecord = entityRecords[entityId.GetEntityIndex()];
+                entityRecord.generation = (entityRecord.generation + 1) & EntityId::GenerationsMask;
+                freeIndices.push_back(entityId.GetEntityIndex());
+            }
+        }
+    };
+}
