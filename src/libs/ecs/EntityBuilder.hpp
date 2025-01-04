2435b7945637a272f1e663d81fd941d2fc69c156
Konstantin <jkot25@gmail.com>
2025-01-04 06:17:46 +0400

    ECS: Initial implementation of Entity Bulder

---
+++ src/libs/ecs/EntityBuilder.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "ecs/ForwardDeclarations.hpp"
+#include "ecs/Id.hpp"
+
+namespace RR::Ecs
+{
+    template <typename TupleType>
+    class [[nodiscard]] EntityBuilder;
+
+    template <>
+    class [[nodiscard]] EntityBuilder<void>
+    {
+    public:
+        template <typename Component, typename... ARGS>
+        auto Add(ARGS&&... args) &&;
+
+        inline void Commit() &&;
+
+    private:
+        friend World;
+
+        explicit EntityBuilder(World& world) : world_ {world} { };
+
+    private:
+        World& world_;
+    };
+
+    template <typename TupleType>
+    class [[nodiscard]] EntityBuilder
+    {
+    public:
+        template <typename Component, typename... ARGS>
+        auto Add(ARGS&&... args) && {
+            using ComponentArgType = ComponentArg<Component, eastl::tuple<ARGS...>>;
+            ComponentArgType arg {eastl::tuple<ARGS...>(eastl::forward<ARGS>(args)...)};
+            using NewTupleType = decltype(eastl::tuple_cat(eastl::declval<TupleType>(), eastl::make_tuple(eastl::move(arg))));
+            return EntityBuilder<NewTupleType>(world_, eastl::tuple_cat(eastl::move(args_), eastl::make_tuple(eastl::move(arg))));
+        }
+
+        inline void Commit() &&;
+    private:
+        template <typename U>
+        friend class EntityBuilder;
+
+        explicit EntityBuilder(World& world, TupleType&& args) : world_(world), args_(eastl::move(args)) { };
+
+    private:
+        World& world_;
+        TupleType args_;
+    };
+
+    template <typename Component, typename... ARGS>
+    auto EntityBuilder<void>::Add(ARGS&&... args) &&
+    {
+        using ComponentArgType = ComponentArg<Component, eastl::tuple<ARGS...>>;
+        ComponentArgType arg {eastl::tuple<ARGS...>(eastl::forward<ARGS>(args)...)};
+        using NewTupleType = eastl::tuple<ComponentArgType>;
+        return EntityBuilder<NewTupleType>(world_, eastl::make_tuple(eastl::move(arg)));
+    }
+
+}
