d7af8966420471ea20da4d67b5445bce240c4c14
Konstantin <jkot25@gmail.com>
2025-03-05 05:00:25 +0400

    ECS Move traits tests to ecs tests

--- src/libs/ecs/tests/Traits.cpp
+++ src/libs/ecs/tests/Traits.cpp
@@ -1,5 +1,5 @@
-#include "TypeTraits.hpp"
-
+#include "ecs/TypeTraits.hpp"
+#include "ecs/ComponentTraits.hpp"
 namespace Test
 {
     struct Foo
@@ -17,13 +17,13 @@ class TemplatedFoo
 
 struct Struct0
 {
+    int x = 0;
 };
 
 class Class0
 {
 };
 
-// Sanity checks
 static_assert(RR::Ecs::TypeName<Test::Foo>::string_view() == "Test::Foo");
 static_assert(RR::Ecs::TypeName<Test::Foo::Bar>::string_view() == "Test::Foo::Bar");
 static_assert(RR::Ecs::TypeName<Struct0>::string_view() == "Struct0");
@@ -32,3 +32,7 @@ static_assert(RR::Ecs::TypeName<TemplatedFoo<int>>::string_view() == "TemplatedF
 static_assert(RR::Ecs::TypeName<TemplatedFoo<Test::Foo::Bar>>::string_view() == "TemplatedFoo<Test::Foo::Bar>");
 static_assert(RR::Ecs::TypeName<int>::string_view() == "int");
 static_assert(RR::Ecs::TypeName<float>::string_view() == "float");
+
+static_assert(RR::Ecs::IsTag<Struct0> == false);
+static_assert(RR::Ecs::IsTag<Class0> == true);
+static_assert(RR::Ecs::IsTag<TemplatedFoo<int>> == true);
