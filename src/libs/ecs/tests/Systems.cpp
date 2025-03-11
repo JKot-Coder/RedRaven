752c230688becebd058766ad664fde62b87ad1b2
Konstantin <jkot25@gmail.com>
2025-03-11 19:26:38 +0400

    ECS: Event args tests

--- src/libs/ecs/tests/Systems.cpp
+++ src/libs/ecs/tests/Systems.cpp
@@ -13,6 +13,12 @@ struct TestEvent : Event
     TestEvent() : RR::Ecs::Event(GetComponentId<TestEvent>, sizeof(TestEvent)) { };
 };
 
+struct IntEvent : Event
+{
+    IntEvent(int value) : RR::Ecs::Event(GetComponentId<IntEvent>, sizeof(IntEvent)), value(value) { };
+    int value;
+};
+
 TEST_CASE_METHOD(WorldFixture, "System create", "[System]")
 {
     world.System().OnEvent<TestEvent>([]{});
@@ -20,7 +26,7 @@ TEST_CASE_METHOD(WorldFixture, "System create", "[System]")
     world.System().OnEvent<TestEvent>([]{});
 }
 
-TEST_CASE_METHOD(WorldFixture, "System create2", "[System]")
+TEST_CASE_METHOD(WorldFixture, "System event", "[System]")
 {
     world.Entity().Edit().Add<int>().Apply();
     world.Entity().Edit().Add<int>().Apply();
@@ -30,4 +36,29 @@ TEST_CASE_METHOD(WorldFixture, "System create2", "[System]")
     world.Event<TestEvent>().EmitImmediately(TestEvent{});
 
     REQUIRE(calls == 2);
+}
+
+TEST_CASE_METHOD(WorldFixture, "System event data", "[System]")
+{
+    world.Entity().Edit().Add<int>().Apply();
+
+    int calls = 0;
+    eastl::array<int, 4> data;
+    const auto sustem = world.System().Require<int>().OnEvent<IntEvent>(
+        [&calls, &data](EntityId id, const IntEvent& event) {
+            REQUIRE(id);
+            data[calls++] = event.value;
+        });
+
+    UNUSED(sustem);
+    world.Event<IntEvent>().EmitImmediately(IntEvent {1});
+    world.Event<IntEvent>().EmitImmediately(IntEvent {2});
+    world.Event<IntEvent>().EmitImmediately(IntEvent {3});
+    world.Event<IntEvent>().EmitImmediately(IntEvent {4});
+
+    REQUIRE(calls == 4);
+    REQUIRE(data[0] == 1);
+    REQUIRE(data[1] == 2);
+    REQUIRE(data[2] == 3);
+    REQUIRE(data[3] == 4);
 }
