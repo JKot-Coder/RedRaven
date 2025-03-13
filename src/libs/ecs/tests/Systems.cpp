9dddcf3a5499a4f052470d413a155112fe70423a
Konstantin <jkot25@gmail.com>
2025-03-13 20:27:04 +0400

    ECS: Add new system tests

--- src/libs/ecs/tests/Systems.cpp
+++ src/libs/ecs/tests/Systems.cpp
@@ -19,6 +19,12 @@ struct IntEvent : Event
     int value;
 };
 
+struct FloatEvent : Event
+{
+    FloatEvent(float value) : RR::Ecs::Event(GetComponentId<FloatEvent>, sizeof(FloatEvent)), value(value) { };
+    float value;
+};
+
 TEST_CASE_METHOD(WorldFixture, "System create", "[System]")
 {
     world.System().OnEvent<TestEvent>([]{});
@@ -38,19 +44,19 @@ TEST_CASE_METHOD(WorldFixture, "System event", "[System]")
     REQUIRE(calls == 2);
 }
 
-TEST_CASE_METHOD(WorldFixture, "System event data", "[System]")
+TEST_CASE_METHOD(WorldFixture, "System immediate  event data", "[System]")
 {
     world.Entity().Edit().Add<int>().Apply();
 
     int calls = 0;
     eastl::array<int, 4> data;
-    const auto sustem = world.System().Require<int>().OnEvent<IntEvent>(
+    world.System().Require<int>().OnEvent<IntEvent>(
         [&calls, &data](EntityId id, const IntEvent& event) {
             REQUIRE(id);
             data[calls++] = event.value;
         });
 
-    UNUSED(sustem);
+
     world.Event<IntEvent>().EmitImmediately(IntEvent {1});
     world.Event<IntEvent>().EmitImmediately(IntEvent {2});
     world.Event<IntEvent>().EmitImmediately(IntEvent {3});
@@ -62,3 +68,36 @@ TEST_CASE_METHOD(WorldFixture, "System event data", "[System]")
     REQUIRE(data[2] == 3);
     REQUIRE(data[3] == 4);
 }
+
+TEST_CASE_METHOD(WorldFixture, "System deffered event data", "[System]")
+{
+    world.Entity().Edit().Add<int>().Apply();
+
+    int calls = 0;
+    eastl::array<int, 4> data;
+    world.System().Require<int>().OnEvent<IntEvent>(
+        [&calls, &data](EntityId id, const IntEvent& event) {
+            REQUIRE(id);
+            data[calls++] = event.value;
+        });
+
+    world.System().Require<int>().OnEvent<FloatEvent>(
+        [&calls, &data](EntityId id, const FloatEvent& event) {
+            REQUIRE(id);
+            data[calls++] = event.value;
+        });
+
+    world.Event<IntEvent>().Emit(IntEvent {1});
+    world.Event<FloatEvent>().Emit(FloatEvent {2});
+    world.Event<FloatEvent>().Emit(FloatEvent {3});
+    world.Event<IntEvent>().Emit(IntEvent {4});
+
+    world.ProcessDefferedEvents();
+
+    REQUIRE(calls == 4);
+    REQUIRE(data[0] == 1);
+    REQUIRE(data[1] == 2);
+    REQUIRE(data[2] == 3);
+    REQUIRE(data[3] == 4);
+}
+
