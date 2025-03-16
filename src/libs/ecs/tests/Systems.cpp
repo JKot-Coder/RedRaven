d72ef9b8fa3f7599d6db4c5fd628cfec10e51472
Konstantin <jkot25@gmail.com>
2025-03-16 05:42:31 +0400

    ECS Unicast events tests

--- src/libs/ecs/tests/Systems.cpp
+++ src/libs/ecs/tests/Systems.cpp
@@ -101,3 +101,42 @@ TEST_CASE_METHOD(WorldFixture, "Broadcast event deffered", "[Event]")
     REQUIRE(data[3] == 4);
 }
 
+TEST_CASE_METHOD(WorldFixture, "Unicast event immediate", "[Event]")
+{
+    const auto entt1 = world.Entity().Edit().Add<int>(1).Apply();
+    const auto entt2 = world.Entity().Edit().Add<int>(2).Apply();
+    const auto entt3 = world.Entity().Edit().Add<int>(3).Apply();
+    const auto entt4 = world.Entity().Edit().Add<int>(4).Apply();
+
+    int calls = 0;
+    eastl::array<std::pair<EntityId, int>, 5> data;
+    world.System().Require<int>().OnEvent<IntEvent, TestEvent>(
+        [&](EntityId id, const Event& event) {
+            data[calls++] = {id, event.Is<IntEvent>() ? event.As<IntEvent>().value : -1};
+        });
+    world.System().Require<float>().OnEvent<IntEvent>([]() { REQUIRE(false); });
+    world.System().Require<int>().OnEvent<FloatEvent, TestEvent>(
+        [&](EntityId id, const Event& event) {
+            data[calls++] = {id, event.Is<FloatEvent>() ? event.As<FloatEvent>().value : -1};
+        });
+
+    world.Event<IntEvent>().Entity(entt3).EmitImmediately(IntEvent {1});
+    world.Event<IntEvent>().Entity(entt2).EmitImmediately(IntEvent {2});
+    world.Event<TestEvent>().Entity(entt4).EmitImmediately({});
+    world.Event<FloatEvent>().Entity(entt1).EmitImmediately(FloatEvent {3});
+
+    REQUIRE(calls == 5);
+    REQUIRE(data[0].first == entt3.GetId());
+    REQUIRE(data[0].second == 1);
+    REQUIRE(data[1].first == entt2.GetId());
+    REQUIRE(data[1].second == 2);
+    REQUIRE(data[2].first == entt4.GetId());
+    REQUIRE(data[2].second == -1);
+    REQUIRE(data[3].first == entt4.GetId());
+    REQUIRE(data[3].second == -1);
+    REQUIRE(data[4].first == entt1.GetId());
+    REQUIRE(data[4].second == 3);
+
+    UNUSED(entt1, entt2, entt3, entt4);
+}
+
