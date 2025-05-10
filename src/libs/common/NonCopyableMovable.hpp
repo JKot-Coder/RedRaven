ce23d9b70b4f53a8572ac533c70f01ee3db75306
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-05-11 01:05:35 +0400

    Refactor NonCopyable and NonMovable classes based on linting

--- src/libs/common/NonCopyableMovable.hpp
+++ src/libs/common/NonCopyableMovable.hpp
@@ -1,27 +1,32 @@
 #pragma once
 
-namespace RR
+namespace RR::Common
 {
-    namespace Common
+    class NonCopyable
     {
-        class NonCopyable
-        {
-        protected:
-            NonCopyable() = default;
-            ~NonCopyable() = default;
+    protected:
+        NonCopyable() = default;
+        ~NonCopyable() = default;
+    public:
+        NonCopyable(const NonCopyable&) = delete;
+        NonCopyable& operator=(const NonCopyable&) = delete;
+        NonCopyable(NonCopyable&&) = default;
+        NonCopyable& operator=(NonCopyable&&) = default;
+    };
 
-            NonCopyable(const NonCopyable&) = delete;
-            NonCopyable& operator=(const NonCopyable&) = delete;
-        };
+    class NonMovable
+    {
+    protected:
+        NonMovable() = default;
+        ~NonMovable() = default;
+    public:
+        NonMovable(const NonMovable&) = default;
+        NonMovable& operator=(const NonMovable&) = default;
+        NonMovable(NonMovable&&) = delete;
+        NonMovable& operator=(NonMovable&&) = delete;
+    };
 
-        class NonMovable
-        {
-        protected:
-            NonMovable() = default;
-            ~NonMovable() = default;
-
-            NonMovable(NonMovable&&) = delete;
-            NonMovable& operator=(NonMovable&&) = delete;
-        };
-    }
+    class NonCopyableMovable : public NonCopyable, public NonMovable
+    {
+    };
 }
