2306b3583fcc430c0d58570d0d7dcb1630f0577a
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-06-29 03:42:05 +0400

    ECS: Enhance tracking tests with immediate and deferred execution scenarios, add new tracking tests

--- src/libs/ecs/tests/Tracking.cpp
+++ src/libs/ecs/tests/Tracking.cpp
@@ -8,6 +8,35 @@ struct WorldFixture
     World world;
 };
 
+namespace {
+    template<typename Callable, typename CallableCheck>
+    void immediateTest(Callable&& call, CallableCheck&& check)
+    {
+        World world;
+        call(world);
+        check(world);
+    }
+
+    template<typename Callable, typename CallableCheck>
+    void defferedTest(Callable&& call, CallableCheck&& check)
+    {
+        World world;
+
+        struct SingleExecutionToken{};
+        world.Entity().Add<SingleExecutionToken>().Apply();
+
+        bool called = false;
+        world.View().With<SingleExecutionToken>().ForEach([&] {
+            call(world);
+            called = true;
+        });
+
+        check(world);
+
+        REQUIRE(called);
+    }
+}
+
 template <typename T>
 struct TrackableType
 {
@@ -20,89 +49,146 @@ struct TrackableType
 };
 
 using TrackableInt = TrackableType<int>;
-using TrackableFloat = TrackableType<float>;
+using TrackableUInt = TrackableType<uint32_t>;
 
 TEST_CASE_METHOD(WorldFixture, "Simple tracking", "[Tracking]")
 {
-    eastl::vector<int> results;
+    auto test = [&](World& world) {
+        world.Entity().Add<TrackableInt>(1).Apply();
+    };
 
-    world.Entity().Add<TrackableInt>(1).Apply();
-    world.System().Track<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
-        results.push_back(trackableInt.x);
-    });
+    auto check = [&](World& world) {
+        world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            trackableInt.x = 2;
+        });
 
-    world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
-        trackableInt.x = 2;
-    });
+        world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            trackableInt.x = 3;
+        });
 
-    world.OrderSystems();
-    world.ProcessTrackedChanges();
+        eastl::vector<int> results;
+        world.System().Track<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            results.push_back(trackableInt.x);
+        });
 
-    REQUIRE(results.size() == 1);
-    REQUIRE(results[0] == 2);
+        world.OrderSystems();
+        world.ProcessTrackedChanges();
+        REQUIRE(results.size() == 1);
+        REQUIRE(results[0] == 3);
+    };
+
+    SECTION("Immediate") { immediateTest(test, check); }
+    SECTION("Deffered") { defferedTest(test, check); }
+}
+
+TEST_CASE_METHOD(WorldFixture, "Delete tracked component", "[Tracking]")
+{
+    auto test = [&](World& world) {
+        auto entity = world.Entity().Add<TrackableInt>(1).Apply();
+        world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            trackableInt.x = 2;
+        });
+
+      entity.Edit().Remove<TrackableInt>().Apply();
+    };
+
+    auto check = [&](World& world) {
+        eastl::vector<int> results;
+        world.System().Track<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            results.push_back(trackableInt.x);
+        });
+
+        world.OrderSystems();
+        world.ProcessTrackedChanges();
+        REQUIRE(results.size() == 0);
+    };
+
+    SECTION("Immediate") { immediateTest(test, check); }
+    SECTION("Deffered") { defferedTest(test, check); }
 }
 
 TEST_CASE_METHOD(WorldFixture, "Move tracked components", "[Tracking]")
 {
-    struct DummyTag {};
-    eastl::vector<int> results;
+    struct DummyTag
+    {
+    };
 
-    world.System().Track<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
-        results.push_back(trackableInt.x);
-    });
-    world.OrderSystems();
+    auto test = [&](World& world) {
+        world.Entity().Add<TrackableInt>(1).Add<DummyTag>().Apply();
+        auto entity2 = world.Entity().Add<TrackableInt>(2).Add<DummyTag>().Apply();
+        world.Entity().Add<TrackableInt>(3).Add<DummyTag>().Apply();
 
-    world.Entity().Add<TrackableInt>(1).Add<DummyTag>().Apply();
-    auto entity2 = world.Entity().Add<TrackableInt>(2).Add<DummyTag>().Apply();
-    world.Entity().Add<TrackableInt>(3).Add<DummyTag>().Apply();
-    world.ProcessTrackedChanges(); // No changes
+        // We move entity2 to another archetype and entity3 to position of entity2
+        entity2.Edit().Remove<DummyTag>().Apply();
+    };
 
-    world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
-        trackableInt.x++;
-    });
+    auto check = [&](World& world) {
+        eastl::vector<int> results;
+        world.System().Track<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            results.push_back(trackableInt.x);
+        });
+        world.OrderSystems();
+        world.ProcessTrackedChanges(); // No changes
 
-    // We move entity2 to another archetype and entity3 to position of entity2
-    entity2.Edit().Remove<DummyTag>().Apply();
-    world.ProcessTrackedChanges();
+        world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            trackableInt.x++;
+        });
+        world.ProcessTrackedChanges();
 
-    REQUIRE(results.size() == 3);
-    REQUIRE(results[0] == 2);
-    REQUIRE(results[1] == 4);
-    REQUIRE(results[2] == 3);
+        REQUIRE(results.size() == 3);
+        REQUIRE(results[0] == 2);
+        REQUIRE(results[1] == 4);
+        REQUIRE(results[2] == 3);
+    };
+
+    SECTION("Immediate") { immediateTest(test, check); }
+    SECTION("Deffered") { defferedTest(test, check); }
 }
 
-TEST_CASE_METHOD(WorldFixture, "Deffered creation check", "[Tracking]")
+TEST_CASE_METHOD(WorldFixture, "Multiple tracked components", "[Tracking]")
 {
-    struct DummyTag {};
-    eastl::vector<int> results;
+    auto test = [&](World& world) {
+        world.Entity().Add<TrackableInt>(1).Add<TrackableUInt>(1U).Apply();
+        auto entity2 = world.Entity().Add<TrackableInt>(2).Add<TrackableUInt>(2U).Apply();
+        world.Entity().Add<TrackableInt>(3).Add<TrackableUInt>(3U).Apply();
+        world.Entity().Add<TrackableUInt>(4U).Apply();
 
-    world.System().Track<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
-        results.push_back(trackableInt.x);
-    });
-    world.OrderSystems();
+        // We move entity2 to another archetype and entity3 to position of entity2
+        entity2.Edit().Remove<TrackableUInt>().Apply();
+    };
 
-    EntityId entity2;
+    auto check = [&](World& world) {
+        eastl::vector<int> results;
 
-    world.Entity().Add<DummyTag>().Apply();
-    world.View().With<DummyTag>().ForEach([&entity2](World& world) {
-        std::array<ComponentId, 2> componentIds = {GetComponentId<TrackableInt>, GetComponentId<DummyTag>};
-        UNUSED(componentIds);
-        world.Entity().Add<TrackableInt>(1).Add<DummyTag>().Apply();
-        entity2 = world.Entity().Add<TrackableInt>(2).Add<DummyTag>().Apply().GetId();
-        world.Entity().Add<TrackableInt>(3).Add<DummyTag>().Apply();
-    });
-    world.ProcessTrackedChanges(); // No changes
+        world.System().Track<TrackableInt>().Without<TrackableUInt>().ForEach([&](TrackableInt& trackableInt) {
+            results.push_back(trackableInt.x);
+        });
 
-    world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
-        trackableInt.x++;
-    });
+        world.System().Track<TrackableUInt>().Without<TrackableInt>().ForEach([&](TrackableUInt& trackableUInt) {
+            results.push_back(trackableUInt.x);
+        });
 
-    // We move entity2 to another archetype and entity3 to position of entity2
-    world.GetEntity(entity2).Edit().Remove<DummyTag>().Apply();
-    world.ProcessTrackedChanges();
+        world.System().Track<TrackableInt, TrackableUInt>().ForEach([&](TrackableInt& trackableInt, TrackableUInt& trackableUInt) {
+            REQUIRE(trackableInt.x == int(trackableUInt.x));
+            results.push_back(trackableInt.x);
+        });
+        world.OrderSystems();
 
-    REQUIRE(results.size() == 3);
-    REQUIRE(results[0] == 2);
-    REQUIRE(results[1] == 4);
-    REQUIRE(results[2] == 3);
+        world.View().With<TrackableInt>().ForEach([&](TrackableInt& trackableInt) {
+            trackableInt.x++;
+        });
+
+        world.View().With<TrackableUInt>().ForEach([&](TrackableUInt& trackableUInt) {
+            trackableUInt.x++;
+        });
+        world.ProcessTrackedChanges();
+        REQUIRE(results.size() == 4);
+        REQUIRE(results[0] == 2);
+        REQUIRE(results[1] == 4);
+        REQUIRE(results[2] == 5);
+        REQUIRE(results[3] == 3);
+    };
+
+    SECTION("Immediate") { immediateTest(test, check); }
+    SECTION("Deffered") { defferedTest(test, check); }
 }
