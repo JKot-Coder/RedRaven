d29d87638216482713c62c31993818fc4b1181d1
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-08-12 21:15:15 +0400

    Demo: Add WindowModule dependency and Init it.

--- src/demo2/Application.cpp
+++ src/demo2/Application.cpp
@@ -2,11 +2,15 @@
 
 #include "ecs/Ecs.hpp"
 
+#include "ecs_window/Window.hpp"
+
 namespace RR::App
 {
     int Application::Run()
     {
         Ecs::World world;
+
+        Ecs::WindowModule::Init(world);
 
         while (true)
         {
