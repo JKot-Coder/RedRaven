ab280922bb63cc976e4455d17b3aeb6b0ca71455
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-08-12 21:48:17 +0400

    Demo: Implement application quit functionality

--- src/demo2/Application.cpp
+++ src/demo2/Application.cpp
@@ -6,17 +6,39 @@
 
 namespace RR::App
 {
+    struct Application
+    {
+        ECS_SINGLETON;
+        static bool quit;
+    };
+
+    bool Application::quit = false;
+
+    struct MainWindow{};
+    struct Quit : public Ecs::Event
+    {
+        Quit() : Event(Ecs::GetEventId<Quit>, sizeof(Quit)) { }
+    };
+
+    void Init(Ecs::World& world)
+    {
+        world.System().OnEvent<Quit>().With<Application>().ForEach([]() {
+            Application::quit = true;
+        });
+    }
+
     int RunApplication()
     {
         Ecs::World world;
-
+        Init(world);
         Ecs::WindowModule::Init(world);
 
         world.OrderSystems();
 
-        world.Entity().Add<Ecs::WindowModule::Window>().Apply();
+        world.Entity().Add<Ecs::WindowModule::Window>().Add<MainWindow>().Apply();
+        world.Entity().Add<Application>().Apply();
 
-        while (true)
+        while (!Application::quit)
         {
             world.EmitImmediately<Ecs::WindowModule::Tick>({});
             world.Tick();
