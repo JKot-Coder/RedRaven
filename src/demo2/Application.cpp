5021bc696b969dab4d66d4479726a195c18c30c6
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-08-12 20:14:29 +0400

    Demo: Integrate ECS into Application class and update CMakeLists to include ecs library

--- src/demo2/Application.cpp
+++ src/demo2/Application.cpp
@@ -1,9 +1,18 @@
 #include "Application.hpp"
 
+#include "ecs/Ecs.hpp"
+
 namespace RR::App
 {
     int Application::Run()
     {
+        Ecs::World world;
+
+        while (true)
+        {
+            world.Tick();
+        }
+
         return 0;
     }
 }
