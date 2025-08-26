6e7991c24aae335134f5c1c3aa5a2b33f0dc49ec
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-08-26 16:51:47 +0400

    Common: Change hash type from uint64_t to uint32_t and update hashing functions accordingly

--- src/libs/common/hash/Default.hpp
+++ src/libs/common/hash/Default.hpp
@@ -5,13 +5,13 @@
 
 namespace RR::Common::Hash::Default
 {
-    using HashType = uint64_t;
+    using HashType = uint32_t;
 
-    constexpr inline HashType Hash(const std::string& str) { return RR::Common::Hash::Wyhash::Hash<64>(str.c_str(), (uint32_t)str.size()); }
-    constexpr inline HashType Hash(const void* data, std::size_t len) { return RR::Common::Hash::Wyhash::Hash<64>(data, (uint32_t)len); }
+    constexpr inline HashType Hash(const std::string& str) { return RR::Common::Hash::Wyhash::Hash<32>(str.c_str(), (uint32_t)str.size()); }
+    constexpr inline HashType Hash(const void* data, std::size_t len) { return RR::Common::Hash::Wyhash::Hash<32>(data, (uint32_t)len); }
 
     constexpr inline HashType operator"" _h(const char* str, std::size_t len)
     {
-        return RR::Common::Hash::Wyhash::ForceConstexprHash<64>(str, (uint32_t)len);
+        return RR::Common::Hash::Wyhash::ForceConstexprHash<32>(str, (uint32_t)len);
     }
 }
