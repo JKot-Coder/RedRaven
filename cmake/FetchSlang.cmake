8acf98b446b66b213261d9090181b82af194771f
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-09-05 19:49:22 +0400

    Update SLANG_VERSION in FetchSlang.cmake to 2025.16

--- cmake/FetchSlang.cmake
+++ cmake/FetchSlang.cmake
@@ -43,7 +43,7 @@
 #############################################
 # Options
 
-set(SLANG_VERSION "2024.14.4" CACHE STRING "Version of the Slang release to use, without the leading 'v'. Must correspond to a release available at https://github.com/shader-slang/slang/releases (or whatever the SLANG_MIRROR variable is set to).")
+set(SLANG_VERSION "2025.16" CACHE STRING "Version of the Slang release to use, without the leading 'v'. Must correspond to a release available at https://github.com/shader-slang/slang/releases (or whatever the SLANG_MIRROR variable is set to).")
 set(SLANG_MIRROR "https://github.com/shader-slang/slang" CACHE STRING "This is the source from which release binaries are fetched, set by default to official Slang repository but you may change it to use a fork.")
 
 #############################################
