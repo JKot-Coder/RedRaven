d494b9fed6eec17dbfa1ccf0040229333ed0e4f3
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-09-09 01:23:14 +0400

    Add EffectManager class for managing effects

---
+++ src/libs/render/EffectManager.cpp
@@ -0,0 +1,79 @@
+#include "EffectManager.hpp"
+
+#include "render/DeviceContext.hpp"
+#include "render/Effect.hpp"
+
+#include "effect_library/EffectLibrary.hpp"
+#include "effect_library/EffectFormat.hpp"
+
+#include "common/Result.hpp"
+#include "common/hashing/Default.hpp"
+
+namespace RR::Render
+{
+    EffectManager::EffectManager() {};
+    EffectManager::~EffectManager() {};
+
+    Common::RResult EffectManager::Init(std::string_view path)
+    {
+        ASSERT(!effectLibrary);
+
+        effectLibrary = eastl::make_unique<EffectLibrary::EffectLibrary>();
+        RR_RETURN_ON_FAIL(effectLibrary->Load(path));
+
+        return Common::RResult::Ok;
+    }
+
+    eastl::unique_ptr<Effect> EffectManager::Load(const std::string& name)
+    {
+        ASSERT(effectLibrary);
+
+        auto& deviceContext = DeviceContext::Instance();
+
+        EffectLibrary::EffectDesc libraryEffectDesc;
+
+        // TODO, Could be replace with const expr hash. Todo replace with hashed string
+        if(!effectLibrary->GetEffectDesc(Common::Hashing::Default::Hash(name), libraryEffectDesc))
+        {
+            LOG_ERROR("Failed to get effect desc for effect: {}", name);
+            return nullptr;
+        }
+
+        for(size_t i = 0; i < effectLibrary->GetShaderCount(); i++)
+        {
+            GAPI::ShaderDesc shaderDesc;
+            EffectLibrary::ShaderDesc shaderAssetDesc = effectLibrary->GetShader(i);
+
+            shaderDesc.type = shaderAssetDesc.type;
+            shaderDesc.data = shaderAssetDesc.data;
+            shaderDesc.size = shaderAssetDesc.size;
+
+            shaders.emplace_back(eastl::move(deviceContext.CreateShader(shaderDesc, shaderAssetDesc.name)));
+        }
+
+        Render::EffectDesc effectDesc;
+        for(auto& pass : libraryEffectDesc.passes)
+        {
+            Render::EffectDesc::PassDesc passDesc;
+            passDesc.name = pass.name;
+            passDesc.rasterizerDesc = pass.rasterizerDesc;
+            passDesc.depthStencilDesc = pass.depthStencilDesc;
+            passDesc.blendDesc = pass.blendDesc;
+
+            static_assert(eastl::tuple_size<decltype(passDesc.shaders)>::value == eastl::tuple_size<decltype(pass.shaderIndexes)>::value);
+            for(uint32_t i = 0; i < pass.shaderIndexes.size(); i++)
+            {
+                const auto shaderIndex = pass.shaderIndexes[i];
+                if (shaderIndex == EffectLibrary::Asset::INVALID_SHADER_INDEX)
+                    continue;
+
+                passDesc.shaders[i] = shaders[shaderIndex].get();
+            }
+
+            effectDesc.passes.emplace_back(eastl::move(passDesc));
+        }
+
+        auto effect = deviceContext.CreateEffect(name, eastl::move(effectDesc));
+        return effect;
+    }
+}
---
+++ src/libs/render/EffectManager.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "common/Singleton.hpp"
+
+#include "gapi/ForwardDeclarations.hpp"
+
+namespace RR::EffectLibrary
+{
+    class EffectLibrary;
+}
+
+namespace RR::Common
+{
+    enum class RResult : int32_t;
+}
+
+namespace RR::Render
+{
+    class Effect;
+
+    class EffectManager : public Common::Singleton<EffectManager>
+    {
+    public:
+        EffectManager();
+        ~EffectManager();
+
+        Common::RResult Init(std::string_view path);
+        eastl::unique_ptr<Effect> Load(const std::string& name);
+
+        size_t GetShaderCount() const { return shaders.size(); }
+
+    private:
+        eastl::unique_ptr<EffectLibrary::EffectLibrary> effectLibrary;
+        eastl::vector<eastl::unique_ptr<GAPI::Shader>> shaders;
+    };
+}
