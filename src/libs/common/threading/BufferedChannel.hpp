e1736da9338955a2cf7035b21cfc6235a39d6844
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-09-10 01:57:18 +0400

    Fix BufferChannel codestyle fix namespace name

--- src/libs/common/threading/BufferedChannel.hpp
+++ src/libs/common/threading/BufferedChannel.hpp
@@ -7,89 +7,83 @@
 #include <atomic>
 #include <optional>
 
-namespace RR
+namespace RR::Common::Threading
 {
-    namespace Common
+    template <typename T, std::size_t BufferSize>
+    class BufferedChannel
     {
-        namespace Threading
+    public:
+        BufferedChannel() = default;
+        ~BufferedChannel() = default;
+
+        inline void Put(const T& obj)
         {
-            template <typename T, std::size_t BufferSize>
-            class BufferedChannel
+            if (closed_)
+                return;
+
+            Threading::UniqueLock<Threading::Mutex> lock(mutex_);
+
+            if (buffer_.full())
             {
-            public:
-                BufferedChannel() = default;
-                ~BufferedChannel() = default;
+                outputWait_.wait(lock, [&]() { return !buffer_.full() || closed_; });
+                if (closed_)
+                    return;
+            }
 
-                inline void Put(const T& obj)
-                {
-                    if (closed_)
-                        return;
-
-                    Threading::UniqueLock<Threading::Mutex> lock(mutex_);
-
-                    if (buffer_.full())
-                    {
-                        outputWait_.wait(lock, [&]() { return !buffer_.full() || closed_; });
-                        if (closed_)
-                            return;
-                    }
-
-                    buffer_.push_back(obj);
-                    inputWait_.notify_one();
-                }
-
-                inline std::optional<T> GetNext()
-                {
-                    Threading::UniqueLock<Threading::Mutex> lock(mutex_);
-
-                    if (buffer_.empty())
-                    {
-                        if (closed_)
-                            return std::nullopt;
-
-                        inputWait_.wait(lock, [&]() { return !buffer_.empty() || closed_; });
-
-                        if (buffer_.empty() && closed_)
-                            return std::nullopt;
-                    }
-
-                    const auto temp = std::make_optional<T>(std::move(buffer_.front()));
-                    buffer_.pop_front();
-                    outputWait_.notify_one();
-
-                    return std::move(temp);
-                }
-
-                inline std::optional<T> TryGetNext()
-                {
-                    Threading::UniqueLock<Threading::Mutex> lock(mutex_);
-
-                    if (buffer_.empty())
-                        return std::nullopt;
-
-                    auto temp = std::make_optional<T>(std::move(buffer_.front()));
-                    buffer_.pop_front();
-                    outputWait_.notify_one();
-
-                    return std::move(temp);
-                }
-
-                inline void Close()
-                {
-                    closed_ = true;
-                    inputWait_.notify_all();
-                    outputWait_.notify_all();
-                }
-
-                inline bool IsClosed() const { return closed_; }
-
-            private:
-                std::atomic<bool> closed_ = false;
-                Threading::ConditionVariable inputWait_;
-                Threading::ConditionVariable outputWait_;
-                Threading::Mutex mutex_;
-                RingQueue<T, BufferSize> buffer_;
-            };
+            buffer_.push_back(obj);
+            inputWait_.notify_one();
         }
-    }
+
+        inline std::optional<T> GetNext()
+        {
+            Threading::UniqueLock<Threading::Mutex> lock(mutex_);
+
+            if (buffer_.empty())
+            {
+                if (closed_)
+                    return std::nullopt;
+
+                inputWait_.wait(lock, [&]() { return !buffer_.empty() || closed_; });
+
+                if (buffer_.empty() && closed_)
+                    return std::nullopt;
+            }
+
+            const auto temp = std::make_optional<T>(std::move(buffer_.front()));
+            buffer_.pop_front();
+            outputWait_.notify_one();
+
+            return std::move(temp);
+        }
+
+        inline std::optional<T> TryGetNext()
+        {
+            Threading::UniqueLock<Threading::Mutex> lock(mutex_);
+
+            if (buffer_.empty())
+                return std::nullopt;
+
+            auto temp = std::make_optional<T>(std::move(buffer_.front()));
+            buffer_.pop_front();
+            outputWait_.notify_one();
+
+            return std::move(temp);
+        }
+
+        inline void Close()
+        {
+            closed_ = true;
+            inputWait_.notify_all();
+            outputWait_.notify_all();
+        }
+
+        inline bool IsClosed() const { return closed_; }
+
+    private:
+        std::atomic<bool> closed_ = false;
+        Threading::ConditionVariable inputWait_;
+        Threading::ConditionVariable outputWait_;
+        Threading::Mutex mutex_;
+        RingQueue<T, BufferSize> buffer_;
+    };
 }
