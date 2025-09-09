907bf90ad8e9c4470967f226d01f0da38860a067
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-09-10 03:55:37 +0400

    fix codestyle updating namespace in Event

--- src/libs/common/threading/Event.hpp
+++ src/libs/common/threading/Event.hpp
@@ -2,65 +2,59 @@
 
 #include "common/threading/Mutex.hpp"
 
-namespace RR
+namespace RR::Common::Threading
 {
-    namespace Common
+    const uint32_t INFINITE_WAIT = 0xFFFFFFFF;
+
+    class Event final : public Common::NonCopyableMovable
     {
-        namespace Threading
+    public:
+        Event(bool manualReset = true, bool initialState = false) : state_(initialState), manualReset_(manualReset) { }
+        ~Event() = default;
+
+        inline void Reset()
         {
-            const uint32_t INFINITE_WAIT = 0xFFFFFFFF;
-
-            class Event final : public Common::NonCopyableMovable
-            {
-            public:
-                Event(bool manualReset = true, bool initialState = false) : state_(initialState), manualReset_(manualReset) { }
-                ~Event() = default;
-
-                inline void Reset()
-                {
-                    UniqueLock<Mutex> lock(mutex_);
-                    state_ = false;
-                }
-
-                inline void Notify()
-                {
-                    {
-                        UniqueLock<Mutex> lock(mutex_);
-                        state_ = true;
-                    }
-
-                    condition_.notify_all();
-                }
-
-                inline bool Wait(uint32_t milliseconds = INFINITE_WAIT)
-                {
-                    UniqueLock<Mutex> lock(mutex_);
-                    bool result = true;
-
-                    if (state_ == false)
-                    {
-                        if (milliseconds == INFINITE_WAIT)
-                        {
-                            condition_.wait(lock, [&]() { return state_; });
-                        }
-                        else
-                        {
-                            result = condition_.wait_for(lock, std::chrono::milliseconds(milliseconds), [&]() { return state_; });
-                        }
-                    }
-
-                    if (!manualReset_)
-                        state_ = false;
-
-                    return result;
-                }
-
-            private:
-                Mutex mutex_;
-                std::condition_variable condition_;
-                bool state_ = false;
-                bool manualReset_ = true;
-            };
+            UniqueLock<Mutex> lock(mutex_);
+            state_ = false;
         }
-    }
+
+        inline void Notify()
+        {
+            {
+                UniqueLock<Mutex> lock(mutex_);
+                state_ = true;
+            }
+
+            condition_.notify_all();
+        }
+
+        inline bool Wait(uint32_t milliseconds = INFINITE_WAIT)
+        {
+            UniqueLock<Mutex> lock(mutex_);
+            bool result = true;
+
+            if (state_ == false)
+            {
+                if (milliseconds == INFINITE_WAIT)
+                {
+                    condition_.wait(lock, [&]() { return state_; });
+                }
+                else
+                {
+                    result = condition_.wait_for(lock, std::chrono::milliseconds(milliseconds), [&]() { return state_; });
+                }
+            }
+
+            if (!manualReset_)
+                state_ = false;
+
+            return result;
+        }
+
+    private:
+        Mutex mutex_;
+        std::condition_variable condition_;
+        bool state_ = false;
+        bool manualReset_ = true;
+    };
 }
