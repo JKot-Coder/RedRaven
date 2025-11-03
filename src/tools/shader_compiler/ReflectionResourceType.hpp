e4d0bd7e1709860a703e9484de26985c05f5b7b4
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-11-04 03:09:24 +0400

    Add ENUM_INFO macro for enum reflection in shader compiler
    This commit introduces the ENUM_INFO macro to facilitate the reflection of enums in the shader compiler.

--- src/tools/shader_compiler/ReflectionResourceType.hpp
+++ src/tools/shader_compiler/ReflectionResourceType.hpp
@@ -1,5 +1,25 @@
 #pragma once
 
+/**
+ * Define enum information. This is expected to be used as follows:
+ *
+ * enum class Foo { A, B, C };
+ * ENUM_INFO(Foo, {
+ *     { Foo::A, "A" },
+ *     { Foo::B, "B" },
+ *     { Foo::C, "C" },
+ * })
+ */
+#define ENUM_INFO(T, ...)                                             \
+    struct T##_info                                                   \
+    {                                                                 \
+        static eastl::span<eastl::pair<T, const char*>> items()       \
+        {                                                             \
+            static eastl::pair<T, const char*> items[] = __VA_ARGS__; \
+            return {eastl::begin(items), eastl::end(items)};          \
+        }                                                             \
+    }
+
 namespace RR
 {
     /**
