1d7d2ac1b8bd98c365023f406a341ebc12e75a1a
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-12-25 07:51:49 +0400

    DX12: Implement CommandAllocatorsPool for efficient command allocator management in CommandList2

--- src/libs/gapi_dx12/CommandList2Impl.cpp
+++ src/libs/gapi_dx12/CommandList2Impl.cpp
@@ -1,12 +1,102 @@
 #include "CommandList2Impl.hpp"
 
+#include "gapi_dx12/DeviceContext.hpp"
+
+#include "gapi/CommandList2.hpp"
+
+#include "gapi/commands/SetRenderPass.hpp"
+
+
 #define NOT_IMPLEMENTED() ASSERT_MSG(false, "Not implemented")
 
 namespace RR::GAPI::DX12
 {
+    ComSharedPtr<ID3D12CommandAllocator> CommandList2Impl::CommandAllocatorsPool::createAllocator() const
+    {
+        ComSharedPtr<ID3D12CommandAllocator> allocator;
+
+        D3DCall(DeviceContext::GetDevice()->CreateCommandAllocator(type_, IID_PPV_ARGS(allocator.put())));
+      //  D3DUtils::SetAPIName(allocator.get(), name_, allocators_.size() + 1);
+
+        return allocator;
+    }
+
+    CommandList2Impl::CommandAllocatorsPool::~CommandAllocatorsPool()
+    {
+        while (!allocators_.empty())
+        {
+            auto& allocator = allocators_.front();
+            //ResourceReleaseContext::DeferredD3DResourceRelease(allocator.first);
+
+            allocators_.pop();
+        }
+    }
+
+    void CommandList2Impl::CommandAllocatorsPool::Init(
+        D3D12_COMMAND_LIST_TYPE type,
+        const std::string& name)
+    {
+        UNUSED(name);
+        type_ = type;
+      //  name_ = name;
+        fence_ = eastl::make_unique<FenceImpl>();
+        fence_->Init(name);
+
+        for (uint32_t index = 0; index < InitialAllocatorsCount; index++)
+            allocators_.emplace(createAllocator(), 0);
+    }
+
+    ComSharedPtr<ID3D12CommandAllocator> CommandList2Impl::CommandAllocatorsPool::GetNextAllocator()
+    {
+        auto allocator = allocators_.front();
+
+        if (fence_->GetGpuValue() < allocator.second)
+        {
+            ASSERT_MSG(allocators_.size() < MaxAllocatorsCount,
+                       "Too many allocators. Fence value: {}, Alocator fence value: {}, Allocator count: {}",
+                       fence_->GetGpuValue(), allocator.second, allocators_.size());
+            // The oldest allocator doesn't executed yet, create new one
+            return allocators_.emplace(createAllocator(), fence_->GetCpuValue()).first;
+        }
+
+        // Reuse old one
+        allocator.second = fence_->GetCpuValue();
+        allocator.first->Reset();
+        allocators_.pop();
+        allocators_.push(allocator); // Place in the end
+
+        return allocator.first;
+    }
+
     void CommandList2Impl::Init(const CommandList2& commandList)
     {
+        // TODO proper command list type
+        allocatorsPool.Init(D3D12_COMMAND_LIST_TYPE_DIRECT, commandList.GetName());
         UNUSED(commandList);
-        NOT_IMPLEMENTED();
+    }
+
+    void CommandList2Impl::Compile(CommandList2& commandList)
+    {
+
+        //ASSERT(commandList.size() != 0);
+
+        for (const auto* command : commandList)
+        {
+            switch (command->type)
+            {
+            case Command::Type::SetRenderPass:
+              //  compileCommand(static_cast<const Commands::SetRenderPass&>(*command), ctx);
+                break;
+
+            default:
+                ASSERT_MSG(false, "Unsupported command type");
+                break;
+            }
+        }
+
+        commandList.clear();
+
+       // commandBuffer = commandEncoder.finish();
+        //commandEncoder.release();
     }
 }
--- src/libs/gapi_dx12/CommandList2Impl.hpp
+++ src/libs/gapi_dx12/CommandList2Impl.hpp
@@ -1,11 +1,46 @@
 #pragma once
+
 #include "gapi/CommandList2.hpp"
+
+#include "eastl/queue.h"
+
+#include "gapi_dx12/FenceImpl.hpp"
 
 namespace RR::GAPI::DX12
 {
     class CommandList2Impl final : public ICommandList
     {
+    private:
+        class CommandAllocatorsPool final
+        {
+        public:
+            ~CommandAllocatorsPool();
+
+            void Init(D3D12_COMMAND_LIST_TYPE type, const std::string& name);
+
+            ComSharedPtr<ID3D12CommandAllocator> GetNextAllocator();
+            FenceImpl& GetSubmissionFence() const { return *fence_; }
+
+        private:
+            using AllocatorFecnceValuePair = std::pair<ComSharedPtr<ID3D12CommandAllocator>, uint64_t>;
+            ComSharedPtr<ID3D12CommandAllocator> createAllocator() const;
+
+        private:
+            D3D12_COMMAND_LIST_TYPE type_;
+            eastl::unique_ptr<FenceImpl> fence_;
+            eastl::queue<AllocatorFecnceValuePair> allocators_;
+        };
+
+    private:
+        // One frame executing on GPU
+        static constexpr int InitialAllocatorsCount = MAX_GPU_FRAMES_BUFFERED + 1;
+        static constexpr int MaxAllocatorsCount = 16;
+
     public:
         void Init(const CommandList2& commandList);
+        void Compile(CommandList2& commandList);
+
+    private:
+        CommandAllocatorsPool allocatorsPool;
     };
 }
