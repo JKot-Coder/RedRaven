267291dd7ec507db233a11592448f046cca2bf01
Zhirnokleev Konstantin <jkot25@gmail.com>
2025-12-29 16:01:48 +0400

    WebGPU: Implement shader module creation in ShaderImpl

--- src/libs/gapi_webgpu/ShaderImpl.cpp
+++ src/libs/gapi_webgpu/ShaderImpl.cpp
@@ -10,8 +10,17 @@ namespace RR::GAPI::WebGPU
 
     void ShaderImpl::Init(const wgpu::Device& device, const Shader& resource)
     {
-        UNUSED(device, resource);
-        NOT_IMPLEMENTED();
+        wgpu::ShaderModuleWGSLDescriptor wgslDesc;
+        wgslDesc.chain.sType = WGPUSType_ShaderSourceWGSL;
+        wgslDesc.chain.next = nullptr;
+        wgslDesc.code = wgpu::StringView(std::string_view(reinterpret_cast<const char*>(resource.GetDesc().data), resource.GetDesc().size));
+
+        wgpu::ShaderModuleDescriptor desc;
+        desc.setDefault();
+        desc.label = wgpu::StringView(resource.GetName().c_str());
+        desc.nextInChain = &wgslDesc.chain;
+
+        shaderModule = device.createShaderModule(desc);
     }
 }
 
